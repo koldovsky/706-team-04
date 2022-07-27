c665453aa404c3b99b205a8e08fa0bc2c534cef0
kupinskiy <AndriyKupinskiy@gmail.com>
2022-07-27 16:16:51 +0300

    Add css for counter

--- js/places.js
+++ js/places.js
@@ -8,8 +8,8 @@ const x = setInterval(function() {
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.querySelector(".countdown-timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.querySelector(".countdown-timer").innerHTML = days + "d : " + hours + "h : "
+  + minutes + "m : " + seconds + "s ";
   if (distance < 0) {
     clearInterval(x);
     document.querySelector(".countdown-timer").innerHTML = "Booking is closed! Please come back later!";
