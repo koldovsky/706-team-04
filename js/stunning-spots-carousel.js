fafa045822205bf415ea83a8eb79517a2e73331f
annakynutsya <kunits.anya@gmail.com>
2022-08-04 21:19:00 +0300

    added js to spots slider

--- js/stunning-spots-carousel.js
+++ js/stunning-spots-carousel.js
@@ -1,38 +1,38 @@
-// const spotsSliderContent = document.getElementById('spots__slider-content');
-// const spotsSlides = document.querySelectorAll('.spots__slide');
-// const spotsLeftBtn = document.getElementById('spots-left-arrow');
-// const spotsRightBtn = document.getElementById('spots-right-arrow');
+const spots__content = document.getElementById('spots__content');
+const spotsSlides = document.querySelectorAll('.spots__slide');
+const spotsLeftBtn = document.getElementById('spots-left');
+const spotsRightBtn = document.getElementById('spots-right');
 
-// let spotsActiveSlide = 1;
+let spotsActiveSlide = 0;
 
-// spotsRightBtn.addEventListener('click', () => {
-//     spotsActiveSlide++;
+spotsRightBtn.addEventListener('click', () => {
+    spotsActiveSlide++
 
-//     if (spotsActiveSlide > spotsSlides.length - 1) {
-//         spotsActiveSlide = 0
-//     }
-//     setBgToSpotsSliderContent()
-//     setspotsActiveSlide()
-// })
+    if (spotsActiveSlide > spotsSlides.length - 1) {
+        spotsActiveSlide = 0
+    }
+    setBg()
+    setspotsActiveSlide()
+})
 
-// spotsLeftBtn.addEventListener('click', () => {
-//     spotsActiveSlide--
+spotsLeftBtn.addEventListener('click', () => {
+    spotsActiveSlide--
 
-//     if (spotsActiveSlide < 0) {
-//         spotsActiveSlide = spotsSlides.length - 1
-//     }
-//     setBgToSpotsSliderContent()
-//     setspotsActiveSlide()
-// })
+    if (spotsActiveSlide < 0) {
+        spotsActiveSlide = spotsSlides.length - 1
+    }
+    setBg()
+    setspotsActiveSlide()
+})
 
-// setBgToSpotsSliderContent()
+setBg()
 
-// function setBgToSpotsSliderContent () {
-//     spotsSliderContent.style.backgroundImage = spotsSlides[spotsActiveSlide].style.backgroundImage
-// }
+function setBg () {
+    spots__content.style.backgroundImage = spotsSlides[spotsActiveSlide].style.backgroundImage
+}
 
-// function setspotsActiveSlide() {
-//     spotsSlides.forEach((slide) => slide.classList.remove('active'))
+function setspotsActiveSlide() {
+    spotsSlides.forEach((slide) => slide.classList.remove('active'))
 
-//     spotsSlides[spotsActiveSlide].classList.add('active')
-// }
+    spotsSlides[spotsActiveSlide].classList.add('active')
+}
