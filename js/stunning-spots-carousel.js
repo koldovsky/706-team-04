350dd3a4ef82a11f6063c7937b39c8e07e7073d2
annakynutsya <kunits.anya@gmail.com>
2022-08-04 14:48:50 +0300

    worked on spots slider

--- js/stunning-spots-carousel.js
+++ js/stunning-spots-carousel.js
@@ -1,37 +1,38 @@
-(function () {
-    const prevButton = document.getElementById("spots-carousel__button-prev"),
-      nextButton = document.getElementById("spots-carousel__button-next"),
-      spotsSlides = document.querySelectorAll(".spots__carousel-slide");
-  
-    slideIdx = 0;
-  
-    const activeSlide = (n) => {
-      console.log(n);
-      for (let spotsSlide of spotsSlides) {
-        spotsSlide.classList.remove("active");
-      }
-      spotsSlides[n].classList.add("active");
-    };
-  
-    const spotsNextSlide = () => {
-      if (slideIdx == spotsSlides.length - 1) {
-        slideIdx = 0;
-      } else {
-        slideIdx++;
-        activeSlide(slideIdx);
-      }
-    };
-  
-    const spotsPrevSlide = () => {
-      if (slideIdx == 0) {
-        slideIdx = spotsSlides.length - 1;
-      } else {
-        slideIdx--;
-        activeSlide(slideIdx);
-      }
-    };
-  
-    nextButton.addEventListener('click', spotsNextSlide);
-    prevButton.addEventListener('click', spotsPrevSlide);
-  })();
-  
+// const spotsSliderContent = document.getElementById('spots__slider-content');
+// const spotsSlides = document.querySelectorAll('.spots__slide');
+// const spotsLeftBtn = document.getElementById('spots-left-arrow');
+// const spotsRightBtn = document.getElementById('spots-right-arrow');
+
+// let spotsActiveSlide = 1;
+
+// spotsRightBtn.addEventListener('click', () => {
+//     spotsActiveSlide++;
+
+//     if (spotsActiveSlide > spotsSlides.length - 1) {
+//         spotsActiveSlide = 0
+//     }
+//     setBgToSpotsSliderContent()
+//     setspotsActiveSlide()
+// })
+
+// spotsLeftBtn.addEventListener('click', () => {
+//     spotsActiveSlide--
+
+//     if (spotsActiveSlide < 0) {
+//         spotsActiveSlide = spotsSlides.length - 1
+//     }
+//     setBgToSpotsSliderContent()
+//     setspotsActiveSlide()
+// })
+
+// setBgToSpotsSliderContent()
+
+// function setBgToSpotsSliderContent () {
+//     spotsSliderContent.style.backgroundImage = spotsSlides[spotsActiveSlide].style.backgroundImage
+// }
+
+// function setspotsActiveSlide() {
+//     spotsSlides.forEach((slide) => slide.classList.remove('active'))
+
+//     spotsSlides[spotsActiveSlide].classList.add('active')
+// }
