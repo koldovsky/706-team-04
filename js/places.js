36ede6058fc213772e0fb4e307f955ae11d6d624
kupinskiy <AndriyKupinskiy@gmail.com>
2022-08-07 21:57:07 +0300

    Added a city image switcher

--- js/places.js
+++ js/places.js
@@ -92,8 +92,7 @@ let weather = {
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".pressure").innerText = "Pressure: " + pressure + " hPa";
     document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-  //document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    document.getElementById("bgimg").style.backgroundImage = "url('https://source.unsplash.com/1600x1450/?" + name + "')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
